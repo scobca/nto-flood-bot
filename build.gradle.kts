a602e820a7be5d63df79e77799a77abc7da090ab
Vladimir <115186975+scobca@users.noreply.github.com>
2024-09-27 16:33:38 +0300

    Creating bot-layout, connecting to external repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,12 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://jitpack.io")
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("com.github.elbekD:kt-telegram-bot:2.2.0")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,17 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import config.token
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+import com.elbekd.bot.Bot
+import com.elbekd.bot.model.toChatId
+
+fun main(args: Array<String>) {
+    val bot = Bot.createPolling(token);
+
+    bot.onCommand("/test") { (msg, _) ->
+        bot.sendMessage(
+            chatId = msg.chat.id.toChatId(),
+            text = "Hello",
+        )
+    }
+    bot.start()
+    println(token)
 }
