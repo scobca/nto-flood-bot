b0f239e1c7947428cbb5c7eae4636c92223ae8bd
Vladimir <115186975+scobca@users.noreply.github.com>
2024-09-28 02:54:24 +0300

    Adding ability to delete all messages with E-word
    - when posting message
    - when editing message
    - when posting media with caption
    - when editing media with caption

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,12 @@
 import config.token
 
 import com.elbekd.bot.Bot
+import com.elbekd.bot.model.ChatId
 import com.elbekd.bot.model.toChatId
-import com.elbekd.bot.types.Message
+import com.elbekd.bot.types.*
 
 fun main(args: Array<String>) {
-    val bot = Bot.createPolling(token);
+    val bot = Bot.createPolling(token)
 
     bot.onCommand("/test") { (msg, _) ->
         bot.sendMessage(
@@ -13,15 +14,44 @@ fun main(args: Array<String>) {
             text = "Hello",
         )
     }
-    bot.onMessage { message -> checkNewMessages(message, bot) }
+    bot.onAnyUpdate { action -> actionController(action, bot) }
     bot.start()
     println(token)
 }
 
-suspend fun checkNewMessages(msg: Message, bot: Bot) {
-    val msgID = msg.messageId
-    val msgText = msg.text
-    val chatID = msg.chat.id.toChatId()
+suspend fun validateNewMessages(msgText: String, msgID: Long, chatID: ChatId, bot: Bot) {
+    println(msgText)
 
-    bot.deleteMessage(chatID, msgID);
+    if ("E-word" in msgText) {
+        bot.deleteMessage(chatID, msgID)
+        println(1111)
+    }
+}
+
+suspend fun actionController(action: Update, bot: Bot) {
+    var msgText = ""
+    var msgID: Long = 0
+    var chatId: Long = 0
+
+    if (action is UpdateMessage) {
+        msgText = if (action.message.text != null) {
+            action.message.text.toString()
+        } else {
+            action.message.caption.toString()
+        }
+        msgID = action.message.messageId
+        chatId = action.message.chat.id
+    } else if (action is UpdateEditedMessage) {
+        println(action)
+
+        msgText = if (action.editedMessage.text != null) {
+            action.editedMessage.text.toString()
+        } else {
+            action.editedMessage.caption.toString()
+        }
+        msgID = action.editedMessage.messageId
+        chatId = action.editedMessage.chat.id
+    }
+
+    validateNewMessages(msgText, msgID, chatId.toChatId(), bot)
 }
